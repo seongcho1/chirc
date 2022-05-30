250994b18cae4889994efce0c600e25926097cb4
seongcho <indivisibilibus@gmail.com>
2022-05-30 16:45:39 +0900

    init

---
+++ src/main.c
@@ -0,0 +1,163 @@
+/*
+ *
+ *  chirc: a simple multi-threaded IRC server
+ *
+ *  This module provides the main() function for the server,
+ *  and parses the command-line arguments to the chirc executable.
+ *
+ */
+
+/*
+ *  Copyright (c) 2011-2020, The University of Chicago
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or withsend
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ *  - Neither the name of The University of Chicago nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software withsend specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY send OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#include "log.h"
+
+
+int main(int argc, char *argv[])
+{
+    int opt;
+    char *port = "6667", *passwd = NULL, *servername = NULL, *network_file = NULL;
+    int verbosity = 0;
+
+    while ((opt = getopt(argc, argv, "p:o:s:n:vqh")) != -1)
+        switch (opt)
+        {
+        case 'p':
+            port = strdup(optarg);
+            break;
+        case 'o':
+            passwd = strdup(optarg);
+            break;
+        case 's':
+            servername = strdup(optarg);
+            break;
+        case 'n':
+            if (access(optarg, R_OK) == -1)
+            {
+                printf("ERROR: No such file: %s\n", optarg);
+                exit(-1);
+            }
+            network_file = strdup(optarg);
+            break;
+        case 'v':
+            verbosity++;
+            break;
+        case 'q':
+            verbosity = -1;
+            break;
+        case 'h':
+            printf("Usage: chirc -o OPER_PASSWD [-p PORT] [-s SERVERNAME] [-n NETWORK_FILE] [(-q|-v|-vv)]\n");
+            exit(0);
+            break;
+        default:
+            fprintf(stderr, "ERROR: Unknown option -%c\n", opt);
+            exit(-1);
+        }
+
+    if (!passwd)
+    {
+        fprintf(stderr, "ERROR: You must specify an operator password\n");
+        exit(-1);
+    }
+
+    if (network_file && !servername)
+    {
+        fprintf(stderr, "ERROR: If specifying a network file, you must also specify a server name.\n");
+        exit(-1);
+    }
+
+    /* Set logging level based on verbosity */
+    switch(verbosity)
+    {
+    case -1:
+        chirc_setloglevel(QUIET);
+        break;
+    case 0:
+        chirc_setloglevel(INFO);
+        break;
+    case 1:
+        chirc_setloglevel(DEBUG);
+        break;
+    case 2:
+        chirc_setloglevel(TRACE);
+        break;
+    default:
+        chirc_setloglevel(TRACE);
+        break;
+    }
+
+    /* Your code goes here */
+
+    /* IMPORTANT: Like the oneshot-single.c, we are creating the socket and sockaddr
+     * structures manually. Your solution _must_ use getaddrinfo instead. You can see
+     * examples of this in client.c and in server-pthreads.c */
+
+    int server_socket;
+    int client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    int yes = 1;
+    socklen_t sin_size = sizeof(struct sockaddr_in);
+
+    char *msg = ":bar.example.com 001 user1 :Welcome to the Internet Relay Network user1!user1@foo.example.com\r\n";
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(atoi(port));
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    server_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+    bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    listen(server_socket, 5);
+
+    while(1)
+    {
+        client_socket = accept(server_socket, (struct sockaddr *) &client_addr, &sin_size);
+        send(client_socket, msg, strlen(msg), 0);
+    }
+
+    close(server_socket);
+
+    return 0;
+}
+
