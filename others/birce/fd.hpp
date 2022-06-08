7cf0b40b60b2c1451c8152a4632299f1b9260f64
seongcho <indivisibilibus@gmail.com>
2022-06-08 16:43:07 +0900

    empty User class

--- others/birce/fd.hpp
+++ others/birce/fd.hpp
@@ -4,9 +4,9 @@
 #include <iostream>
 #include <queue>
 
-#define FD_FREE		0
-#define FD_SERV		1
-#define FD_CLIENT	2
+// #define FD_FREE		0
+// #define FD_SERV		1
+// #define FD_CLIENT	2
 
 #define BUF_SIZE	512 //4096
 
@@ -17,12 +17,12 @@
 // }					t_fd;
 
 class User {
- 	//int		type_;
+//  	int		type_;
 
-  public:
-	User() {}
-	//User(int type) : type_(type) {}
-	//int	const &type() const { return type_; }
+//   public:
+// 	User() {}
+// 	User(int type) : type_(type) {}
+// 	int	const &type() const { return type_; }
 };
 
 #endif
